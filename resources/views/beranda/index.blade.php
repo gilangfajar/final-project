55569c521c4487d8e89ebbfd2cfd55dab424b311
gilangfajar <49382999+gilangfajar@users.noreply.github.com>
2020-07-12 17:26:13 +0700

    kel. cuma berdua yg satu afk.

--- resources/views/beranda/index.blade.php
+++ resources/views/beranda/index.blade.php
@@ -8,6 +8,7 @@
             </div>
             <!-- /.col-lg-12 -->
         </div>
+        //mantap mantap
 <div class="row">
     <div class="col-lg-3 col-md-6">
         <div class="panel panel-primary">
